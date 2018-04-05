7bdefbd0657ed2397875e6cbfa540b4c476e960b
smorosz <smorosz@yahoo.com>
2018-04-05 11:00:40 -0500

    Did analysis on who survived by sex and did average on Fare paid

--- Titantic.R
+++ Titantic.R
@@ -1,2 +1,17 @@
 
-print("Hello!")
+getwd()
+
+str(train)
+
+passengers <- train
+
+str(passengers$Sex)
+
+library(dplyr)
+
+sum(passengers$Fare)
+
+passengers%>%
+  group_by(Survived, Sex)%>%
+  summarize( avgFare = mean(Fare), n(), prcnt = n()/891 * 100)%>%
+  arrange(Sex, Survived)
